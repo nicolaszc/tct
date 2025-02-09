b7812e3d49980245f0fedbfa18d55da2d5bd7468
nicolaszc <nzakrzewicz@gmail.com>
2025-02-09 20:22:36 -0300

    gra

--- app/public/wp-content/themes/topclass/catalog.php
+++ app/public/wp-content/themes/topclass/catalog.php
@@ -15,6 +15,17 @@ defined('ABSPATH') || exit;
 get_header();
 $host=$_SERVER['HTTP_HOST'];
 $bkg_color = get_field('body_bg_color');
+if ( have_rows( 'background_gradient' ) ) : 
+  while ( have_rows( 'background_gradient' ) ) : the_row();
+    if(get_sub_field('use_gradient')):
+      $use_gradient=get_sub_field('use_gradient');
+      $color_1=get_sub_field('color_1');
+      $color_2=get_sub_field('color_2');
+      $color_3=get_sub_field('color_3');
+      $color_4=get_sub_field('color_4');
+    endif;
+  endwhile;
+endif;
 $bkg_img = get_field('body_bg');
 $bkg_size = get_field('body_bg_size');
 $bkg_position = get_field('body_bg_pos');
@@ -29,6 +40,9 @@ $bajada = get_field('bajada');
     }
     body{
       background: <?php echo $bkg_color ?>;
+      <?php if($use_gradient): ?>
+        background: linear-gradient(0deg, <?php echo $color_1 ?> 0%, <?php echo $color_2 ?> 40%, <?php echo $color_3 ?> 60%, <?php echo $color_4 ?> 100%); 
+      <?php endif; ?>
       background-image: url(<?php echo $bkg_img ?>); 
       background-repeat: no-repeat;
       background-size: <?php echo $bkg_size ?>;
