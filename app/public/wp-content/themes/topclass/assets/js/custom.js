4518c15be90e561520a88f30e96cba9a1f1c7260
nicolaszc <nzakrzewicz@gmail.com>
2025-02-09 15:01:17 -0300

    owl

--- app/public/wp-content/themes/topclass/assets/js/custom.js
+++ app/public/wp-content/themes/topclass/assets/js/custom.js
@@ -3,3 +3,36 @@ jQuery(function ($) {
     // Do stuff here
 
 }); // jQuery End
+$(document).ready(function() {
+
+    $('.owl-main-banner').owlCarousel({
+      loop:false,
+      autoplay:true,
+      rewind:true,
+      autoplayHoverPause:true,
+      margin:10,
+      nav:false,
+      navText:["<div class='nav-btn prev-slide'><i class='fa-solid fa-chevron-left'></i></div>","<div class='nav-btn next-slide'><i class='fa-solid fa-chevron-right'></i></div>"],
+      dots:true,
+      dotsData:true,
+      startPosition: 4,
+      /*  dotsContainer: '.test', */
+      responsive:{
+          0:{
+              items:1
+          },
+          600:{
+              items:1
+          },
+          1000:{
+              items:1
+          }
+      }
+    })
+  
+    $('.owl-dot').click(function() {
+      $('.owl-dot').trigger('to.owl.carousel', [$(this).index(), 1000]);
+    }) 
+    
+    
+  })
