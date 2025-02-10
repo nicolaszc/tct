f89f62d562a7487ccff32e8b6c7975916fa4e33c
nicolaszc <nzakrzewicz@gmail.com>
2025-02-10 01:13:32 -0300

    g

--- app/public/wp-content/themes/topclass/catalog.php
+++ app/public/wp-content/themes/topclass/catalog.php
@@ -13,52 +13,7 @@
 defined('ABSPATH') || exit;
 
 get_header();
-$host=$_SERVER['HTTP_HOST'];
-$bkg_color = get_field('body_bg_color');
-if ( have_rows( 'background_gradient' ) ) : 
-  while ( have_rows( 'background_gradient' ) ) : the_row();
-      $use_gradient=get_sub_field('use_gradient');
-      $color_1=get_sub_field('color_1');
-      $color_2=get_sub_field('color_2');
-      $color_3=get_sub_field('color_3');
-      $color_4=get_sub_field('color_4');
-  endwhile;
-endif;
-$bkg_img = get_field('body_bg');
-$bkg_size = get_field('body_bg_size');
-$bkg_position = get_field('body_bg_pos');
-$bajada = get_field('bajada');
-?>
 
-  <style>
-    html{
-      min-height:100vh;
-      height: auto;
-    }
-    body{
-      background-color: <?php echo $bkg_color ?>!important;
-      background-image: url(<?php echo $bkg_img ?>); 
-      background-repeat: no-repeat;
-      background-size: <?php echo $bkg_size ?>;
-      background-position: <?php echo $bkg_position ?>;
-      min-height:100vh;
-    }
-    .site-content{
-      <?php if($use_gradient): ?>
-        background: -moz-linear-gradient(0deg, <?php echo $color_1 ?> 0%, <?php echo $color_2 ?> 30%, <?php echo $color_3 ?> 70%, <?php echo $color_4 ?> 100%)!important; 
-        background: -webkit-linear-gradient(0deg, <?php echo $color_1 ?> 0%, <?php echo $color_2 ?> 30%, <?php echo $color_3 ?> 70%, <?php echo $color_4 ?> 100%)!important; 
-        background: linear-gradient(0deg, <?php echo $color_1 ?> 0%, <?php echo $color_2 ?> 30%, <?php echo $color_3 ?> 70%, <?php echo $color_4 ?> 100%)!important; 
-        filter: progid:DXImageTransform.Microsoft.gradient(startColorstr='<?php echo $color_1 ?>', endColorstr='<?php echo $color_4 ?>', GradientType=1)!important;
-      <?php endif; ?>
-    }
-    header, footer{
-      <?php if($use_gradient): ?>
-        background: <?php echo $color_4 ?>!important;
-      <?php else: ?>
-        background:rgb(237, 26, 115)!important;
-      <?php endif; ?>
-    }
-  </style>
   
 
   <div id="content" class="site-content">
